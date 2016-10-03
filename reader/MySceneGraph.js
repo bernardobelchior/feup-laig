bb93b587f421ee4f7837a8f4dc99fabf457c6026
Bernardo <bernardo.belchior1@gmail.com>
2016-10-03 13:32:03 +0100

    Reworkign transformations.

--- reader/MySceneGraph.js
+++ reader/MySceneGraph.js
@@ -4,7 +4,7 @@ function MySceneGraph(filename, scene) {
     // Establish bidirectional references between scene and graph
     this.scene = scene;
     scene.graph = this;
-    
+
     // File reading
     this.reader = new CGFXMLreader();
     this.rootId;
@@ -44,48 +44,7 @@ MySceneGraph.prototype.parseDsx = function(dsx) {
     //NOTE: There cannot be a carriage return between the 'return' keyword and
     //the OR statement, otherwise the functions are not called.
 
-    return (this.parseScene(dsx) || this.parseViews(dsx) || this.parsePrimitives(dsx) || this.parseTransformations(dsx) || this.parseMaterials(dsx));
-}
-
-/**
- *Parses the given tag and returns a Vec3 with the result.
- * Attributes are named x, y and z concatenated with the number.
- *TODO:Check if the read values are valid
- */
-MySceneGraph.prototype.parseVec3 = function(tag, number) {
-    if (!number)
-        number = '';
-    let z = this.reader.getFloat(tag, 'z' + number, true);
-
-    let vec3 = this.parseVec2(tag, number);
-    vec3.push(z);
-
-    return vec3;
-}
-
-/**
- * Parses the vector from the given tag attributes.
- * Attributes are named x and y concatenated with the number.
- */
-MySceneGraph.prototype.parseVec2 = function(tag, number) {
-    if (!number)
-        number = '';
-
-    let x = this.reader.getFloat(tag, 'x' + number, true);
-    let y = this.reader.getFloat(tag, 'y' + number, true);
-
-    return [x, y];
-}
-
-MySceneGraph.prototype.parseRGBA = function(tag) {
-    let r = this.reader.getFloat(tag, 'r', true);
-    let g = this.reader.getFloat(tag, 'g', true);
-    let b = this.reader.getFloat(tag, 'b', true);
-    let a = this.reader.getFloat(tag, 'a', true);
-
-    return [
-        r, g, b, a
-    ];
+    return (this.parseScene(dsx) || this.parseViews(dsx) || this.parseTransformations(dsx) || this.parseMaterials(dsx) || this.parsePrimitives(dsx) || this.parseComponents(dsx));
 }
 
 /**
@@ -123,10 +82,10 @@ MySceneGraph.prototype.parseViews = function(dsx) {
 
         //Parse perspective elements
         var fromTag = perspective.getElementsByTagName('from')[0];
-        var fromVector = this.parseVec3(fromTag);
+        var fromVector = parseVec3(this.reader, fromTag);
 
         var toTag = perspective.getElementsByTagName('to')[0];
-        var toVector = this.parseVec3(toTag);
+        var toVector = parseVec3(this.reader, toTag);
 
         //Sets the default camera
         if (defaultPerspectiveId === id)
@@ -157,16 +116,16 @@ MySceneGraph.prototype.parseMaterials = function(dsx) {
             return ('Material with id ' + id + ' already exists.');
 
         let emission = material.getElementsByTagName('emission')[0];
-        let emissionRGBA = this.parseRGBA(emission);
+        let emissionRGBA = parseRGBA(this.reader, emission);
 
         let ambient = material.getElementsByTagName('ambient')[0];
-        let ambientRGBA = this.parseRGBA(ambient);
+        let ambientRGBA = parseRGBA(this.reader, ambient);
 
         let diffuse = material.getElementsByTagName('diffuse')[0];
-        let diffuseRGBA = this.parseRGBA(diffuse);
+        let diffuseRGBA = parseRGBA(this.reader, diffuse);
 
         let specular = material.getElementsByTagName('specular')[0];
-        let specularRGBA = this.parseRGBA(specular);
+        let specularRGBA = parseRGBA(this.reader, specular);
 
         let shininess = material.getElementsByTagName('shininess')[0];
         let shininessValue = this.reader.getFloat(shininess, 'value', true);
@@ -184,10 +143,55 @@ MySceneGraph.prototype.parseMaterials = function(dsx) {
 }
 
 /**
+ * Parses the components from the dsx root element
+ * And creates the scene graph.
+ */
+MySceneGraph.prototype.parseComponents = function(dsx) {
+    let compsTag = dsx.getElementsByTagName('components')[0];
+    let components = {};
+
+    for (let compTag of compsTag.children) {
+        let id = this.reader.getString(compTag, 'id', true);
+        let component = new Component();
+
+        if (!id)
+            return 'A component must have an id. Please provide one.';
+
+        if (components[id])
+            return ('A component with id ' + id + ' already exists.');
+
+        this.parseComponentTransformations(component, compTag.getElementsByTagName('transformation')[0]);
+        this.parseComponentMaterials(component, compTag.getElementsByTagName('materials')[0]);
+        //parseTextures
+        this.parseComponentChildren(component, compTag.getElementsByTagName('children')[0]);
+
+        components[id] = component;
+    }
+}
+
+MySceneGraph.prototype.parseComponentTransformations = function(component, tag) {
+    for (let transformation of tag.children) {
+        if (transformation.nodeName === 'transformationref') {
+
+        } else {
+            component.addTransformation(parseTransformation(this.reader, transformation));
+        }
+    }
+}
+
+MySceneGraph.prototype.parseComponentMaterials = function(component, tag) {
+
+}
+
+MySceneGraph.prototype.parseComponentChildren = function(component, tag) {
+
+}
+
+/**
   Parses the primitives from the dsx root element.
 */
 MySceneGraph.prototype.parsePrimitives = function(dsx) {
-    var primitives = dsx.getElementsByTagName('primitives')[0];
+    let primitives = dsx.getElementsByTagName('primitives')[0];
 
     for (let primitive of primitives.children) {
         let shape = primitive.children[0];
@@ -201,15 +205,15 @@ MySceneGraph.prototype.parsePrimitives = function(dsx) {
         switch (shape.nodeName) {
             case 'rectangle':
                 object = new Rectangle(this.scene,
-                    this.parseVec2(shape, '1'),
-                    this.parseVec2(shape, '2')
+                    parseVec2(this.reader, shape, '1'),
+                    parseVec2(this.reader, shape, '2')
                 );
                 break;
             case 'triangle':
                 object = new Triangle(this.scene,
-                    this.parseVec3(shape, '1'),
-                    this.parseVec3(shape, '2'),
-                    this.parseVec3(shape, '3'));
+                    parseVec3(this.reader, shape, '1'),
+                    parseVec3(this.reader, shape, '2'),
+                    parseVec3(this.reader, shape, '3'));
                 break;
             case 'cylinder':
                 {
