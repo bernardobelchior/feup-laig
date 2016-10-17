84d35568798fa08389d01dfc33d3c03f6634bb49
Bernardo Belchior <bernardo@wifi-interno.fe.up.pt>
2016-10-17 13:04:08 +0100

    Added new cameras to dsx and added camera switching.

--- reader/Interface.js
+++ reader/Interface.js
@@ -5,10 +5,10 @@
 
 
 function Interface(scene) {
-	//call CGFinterface constructor
-	CGFinterface.call(this);
+    //call CGFinterface constructor
+    CGFinterface.call(this);
 
-  this.scene = scene;
+    this.scene = scene;
 };
 
 Interface.prototype = Object.create(CGFinterface.prototype);
@@ -19,52 +19,52 @@ Interface.prototype.constructor = Interface;
  * @param {CGFapplication} application
  */
 Interface.prototype.init = function(application) {
-	// call CGFinterface init
-	CGFinterface.prototype.init.call(this, application);
+    // call CGFinterface init
+    CGFinterface.prototype.init.call(this, application);
 
-	// init GUI. For more information on the methods, check:
-	//  http://workshop.chromeexperiments.com/examples/gui
+    // init GUI. For more information on the methods, check:
+    //  http://workshop.chromeexperiments.com/examples/gui
 
-	this.gui = new dat.GUI();
+    this.gui = new dat.GUI();
 
-	// add a button:
-	// the first parameter is the object that is being controlled (in this case the scene)
-	// the identifier 'doSomething' must be a function declared as part of that object (i.e. a member of the scene class)
-	// e.g. LightingScene.prototype.doSomething = function () { console.log("Doing something..."); };
+    // add a button:
+    // the first parameter is the object that is being controlled (in this case the scene)
+    // the identifier 'doSomething' must be a function declared as part of that object (i.e. a member of the scene class)
+    // e.g. LightingScene.prototype.doSomething = function () { console.log("Doing something..."); };
 
-//	this.gui.add(this.scene, 'clockAnimation');
+    //	this.gui.add(this.scene, 'clockAnimation');
 
-	// add a group of controls (and open/expand by defult)
+    // add a group of controls (and open/expand by defult)
 
-	//var group=this.gui.addFolder("Options");
-	//group.open();
+    //var group=this.gui.addFolder("Options");
+    //group.open();
 
-	// add two check boxes to the group. The identifiers must be members variables of the scene initialized in scene.init as boolean
-	// e.g. this.option1=true; this.option2=false;
+    // add two check boxes to the group. The identifiers must be members variables of the scene initialized in scene.init as boolean
+    // e.g. this.option1=true; this.option2=false;
 
-	//group.add(this.scene, 'option1');
-	//group.add(this.scene, 'option2');
+    //group.add(this.scene, 'option1');
+    //group.add(this.scene, 'option2');
 
-	// add a slider
-	// must be a numeric variable of the scene, initialized in scene.init e.g.
-	// this.speed=3;
-	// min and max values can be specified as parameters
+    // add a slider
+    // must be a numeric variable of the scene, initialized in scene.init e.g.
+    // this.speed=3;
+    // min and max values can be specified as parameters
 
-/*	this.gui.add(this.scene, 'speed', 0.1, 2);
+    /*	this.gui.add(this.scene, 'speed', 0.1, 2);
 
-	var lightGroup = this.gui.addFolder("Lights");
+    	var lightGroup = this.gui.addFolder("Lights");
 
-	lightGroup.open();
-	lightGroup.add(this.scene, 'light0');
-	lightGroup.add(this.scene, 'light1');
-	lightGroup.add(this.scene, 'light2');
-	lightGroup.add(this.scene, 'light3');
-	lightGroup.add(this.scene, 'light4');
+    	lightGroup.open();
+    	lightGroup.add(this.scene, 'light0');
+    	lightGroup.add(this.scene, 'light1');
+    	lightGroup.add(this.scene, 'light2');
+    	lightGroup.add(this.scene, 'light3');
+    	lightGroup.add(this.scene, 'light4');
 
-	// Choose from named values
-	this.gui.add(this.scene, 'textures', { texture1: 0, texture2: 1, texture3: 2} );*/
+    	// Choose from named values
+    	this.gui.add(this.scene, 'textures', { texture1: 0, texture2: 1, texture3: 2} );*/
 
-	return true;
+    return true;
 };
 
 /**
@@ -103,31 +103,34 @@ Interface.prototype.init = function(application) {
 };*/
 
 Interface.prototype.processKeyDown = function(event) {
-	// call CGFinterface default code (omit if you want to override)
-	CGFinterface.prototype.processKeyDown.call(this,event);
+    // call CGFinterface default code (omit if you want to override)
+    CGFinterface.prototype.processKeyDown.call(this, event);
 
-	// Check key codes e.g. here: http://www.asciitable.com/
-	// or use String.fromCharCode(event.keyCode) to compare chars
-	// for better cross-browser support, you may also check suggestions on using event.which in http://www.w3schools.com/jsref/event_key_keycode.asp
-	/*switch (event.keyCode)
-	{
+    // Check key codes e.g. here: http://www.asciitable.com/
+    // or use String.fromCharCode(event.keyCode) to compare chars
+    // for better cross-browser support, you may also check suggestions on using event.which in http://www.w3schools.com/jsref/event_key_keycode.asp
+    /*switch (event.keyCode)
+    {
 
-	};*/
-  //console.log(event.keyCode);
+    };*/
+    //console.log(event.keyCode);
 };
 
 Interface.prototype.processKeyUp = function(event) {
-	// call CGFinterface default code (omit if you want to override)
-	CGFinterface.prototype.processKeyUp.call(this,event);
+    // call CGFinterface default code (omit if you want to override)
+    CGFinterface.prototype.processKeyUp.call(this, event);
 
-	// Check key codes e.g. here: http://www.asciitable.com/
-	// or use String.fromCharCode(event.keyCode) to compare chars
+    // Check key codes e.g. here: http://www.asciitable.com/
+    // or use String.fromCharCode(event.keyCode) to compare chars
 
-	// for better cross-browser support, you may also check suggestions on using event.which in http://www.w3schools.com/jsref/event_key_keycode.asp
+    // for better cross-browser support, you may also check suggestions on using event.which in http://www.w3schools.com/jsref/event_key_keycode.asp
 
-	switch (event.keyCode) {
-		case (77):	// 'M'
-			this.scene.switchMaterials();
-			break;
-	};
+    switch (event.keyCode) {
+        case (77): // 'M'
+            this.scene.switchMaterials();
+            break;
+        case (86): //'V'
+            this.scene.nextCamera();
+            break;
+    };
 };
