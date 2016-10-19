8de15b933b2afae15a6978342d875bca6892c792
epassos26 <epassos26@gmail.com>
2016-10-19 17:32:02 +0100

    Complete rework of the sphere primitive

--- reader/primitives/Sphere.js
+++ reader/primitives/Sphere.js
@@ -1,25 +1,56 @@
 /**
  * Sphere
- * @param gl {WebGLRenderingContext}
  * @constructor
  */
-function Sphere(scene, radius, slices, stacks) {
-	CGFobject.call(this,scene);
-	this.scene = scene;
-	this.slices = slices;
-	this.stacks = stacks;
+function Sphere(scene,radius,slices, stacks) {
+    CGFobject.call(this,scene);
 
-	this.semisphere = new Semisphere(scene, radius, slices, stacks);
+    this.slices = slices;
+    this.stacks = stacks;
+    this.radius = radius;
+
+    this.initBuffers();
 };
 
 Sphere.prototype = Object.create(CGFobject.prototype);
-Sphere.prototype.constructor=Sphere;
+Sphere.prototype.constructor = Sphere;
 
-Sphere.prototype.display = function() {
-	this.semisphere.display();
+Sphere.prototype.initBuffers = function() {
 
-	this.scene.pushMatrix();
-		this.scene.rotate(Math.PI, 1, 0, 0);
-    this.semisphere.display();
-	this.scene.popMatrix();
-}
+    this.vertices = [];
+    this.normals = [];
+    this.indices = [];
+    this.texCoords = [];
+
+    var theta=(2*Math.PI)/this.slices; // 0-360 deg -- longitude
+    var phi=(Math.PI)/this.stacks; // 0-180 deg -- latitude
+    var n_verts = 0;
+
+
+    for(var i=0; i<= this.slices;i++){
+        for(var j = 0; j <= this.stacks; j++) {
+
+            let x = Math.cos(theta * i) * Math.sin(phi * j);
+            let y = Math.sin(theta * i) * Math.sin(phi * j);
+            let z = Math.cos(phi * j);
+
+            this.vertices.push(this.radius * x,this.radius * y,this.radius * z);
+            n_verts++;
+
+            this.normals.push(x,y,z);
+
+            if(i > 0  && j>0 ){
+                this.indices.push(n_verts - this.stacks - 1, n_verts - 1, n_verts - this.stacks - 2);
+                this.indices.push(n_verts - 1, n_verts - 2, n_verts - this.stacks - 2);
+            }
+
+            this.texCoords.push(0.5 * x + 0.5, 0.5 - 0.5 * y);
+        }
+
+    }
+
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    this.initGLBuffers();
+};
+
+
