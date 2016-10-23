324a3fcfb5e840f79b368df9677aee5340d7bc9d
Bernardo Belchior <bernardo.belchior1@gmail.com>
2016-10-23 12:10:21 +0100

    Updated dsx version.

--- reader/main.js
+++ reader/main.js
@@ -36,7 +36,7 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor)
 
-	var filename=getUrlVars()['file'] || "LAIG_TP1_DSX_T2_G06_v02.dsx";
+	var filename=getUrlVars()['file'] || "LAIG_TP1_DSX_T2_G06_v03.dsx";
 
 	// create and load graph, and associate it to scene.
 	// Check console for loading errors
