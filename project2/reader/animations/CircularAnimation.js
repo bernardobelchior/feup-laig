69f8efae7325da77cb922dad2f10fa5583886235
Bernardo Belchior <bernardo.belchior1@gmail.com>
2016-11-07 11:39:57 +0000

    Added surface demo. Created animations base.

---
+++ project2/reader/animations/CircularAnimation.js
@@ -0,0 +1,5 @@
+class CircularAnimation extends Animation {
+  constructor() {
+    super();
+  }
+}
---
+++ project2/reader/animations/LinearAnimation.js
@@ -0,0 +1,5 @@
+class LinearAnimation extends Animation {
+  constructor() {
+    super();
+  }
+}
