e64600cebc90c4432cc4508583846899d0309bdf
epassos26 <epassos26@gmail.com>
2016-11-14 12:05:20 +0000

    added angle_component function to circular animation

--- project2/reader/animations/CircularAnimation.js
+++ project2/reader/animations/CircularAnimation.js
@@ -3,6 +3,17 @@ class CircularAnimation extends Animation {
     //TODO: Angle the object according to the position
 
 
+    /**
+     *
+     * @param scene
+     * @param id            Id of the animation
+     * @param time          Duration of the animation
+     * @param center        Center of the circle followed by the animation (x,y,z)
+     * @param radius
+     * @param startAng      Angle at which the animation starts
+     * @param rotAng        Final angle of the animation (relative to the starting angle)
+     * @constructor
+     */
     constructor(scene, id, time,center,radius,startAng,rotAng) {
         super(scene,id,time);
 
@@ -10,28 +21,41 @@ class CircularAnimation extends Animation {
         this.radius = radius;
         this.startAng = startAng;
         this.rotAng = rotAng;
-        this.angleTraveled = 0;
+        this.angleTraveled = 0;    // Rotation already completed by the animation
     }
 
-     move(deltaTime) {
-         if(this.angleTraveled === 0)
-             setupAnimation();
+    move(deltaTime) {
+        if(this.totalAnimationRotation === 0)
+            this.setupAnimation();
 
-         let rotationSlice = rotAng / deltaTime;
-         if(rotationSlice + angleTraveled > rotAng)                     //If the animation will make the rotation
-             rotationSlice = rotationSlice + angleTraveled - rotAng;    // exceed what was wanted, make it only rotate to
-                                                                        // completion
-         this.scene.pushMatrix();
+
+        let rotationSlice = rotAng / deltaTime;                                 // Rotation to be done in this iteration
+
+        if(rotationSlice + this.angleTraveled > this.rotAng)                    // If the animation will make the rotation
+            rotationSlice = rotationSlice + this.angleTraveled - this.rotAng;   // exceed what was wanted, make it only rotate to
+                                                                                // completion
+        this.angle_component(rotationSlice + this.angleTraveled);
+
+        this.scene.pushMatrix();
             this.scene.rotate(y,rotationSlice);
-         this.scene.popMatrix();
+        this.scene.popMatrix();
 
-         this.angleTraveled += rotationSlice;
-     }
+        this.angleTraveled += rotationSlice;
+    }
 
-     setupAnimation(){
-         this.scene.pushMatrix();
-            this.scene.translate(Math.sin(this.startAng),0,Math.cos(this.startAng));
+
+
+   setupAnimation(){
+        this.scene.pushMatrix();
+            this.scene.translate(this.radius * Math.sin(this.startAng),0, this.radius * Math.cos(this.startAng));
             this.scene.translate(center[0],center[1],center[2]);
-         this.scene.popMatrix();
-     }
+        this.scene.popMatrix();
+    }
+
+   angle_component(angle){
+        this.scene.pushMatrix();
+            this.scene.rotate(y, angle);
+            this.scene.translate(-this.radius * Math.sin(this.startAng),0, -this.radius * Math.cos(this.startAng));
+        this.scene.popMatrix();
+   }
 }
