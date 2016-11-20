f76a5b2bc02d16c401c93e7d157a380564bf956d
Bernardo Belchior <bernardo.belchior1@gmail.com>
2016-11-20 19:16:57 +0000

    Renamed scene.

--- project2/reader/main.js
+++ project2/reader/main.js
@@ -37,7 +37,7 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor)
 
-	var filename=getUrlVars()['file'] || "LAIG_TP2_DSX_T2_G06_v03.dsx";
+	var filename=getUrlVars()['file'] || "LAIG_TP2_DSX_T2_G06_v01.dsx";
 
 	// create and load graph, and associate it to scene.
 	// Check console for loading errors
